5e7c3e2d1e3b0aff4be36d6f71b8f4e4683db9d5
Justin Huynh <justinhuynh639@gmail.com>
2023-09-23 13:19:39 -0700

    started random operator generator

---
+++ project3.py
@@ -0,0 +1,19 @@
+#randomly generate operands
+import random
+
+OPERATORS = ["+", "-", "/", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+
+#write function that generates a problem
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    #randomly select element from a list
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    print(expr)
+    return expr
+
+generate_problem()
